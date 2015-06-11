499c011939058b92a4c7937f78d3d4657f3e30a7
olgamelnichuk <olkin@ebi.ac.uk>
2015-06-11 08:20:39 +0100

    added scripts to install irods 4.1.2

---
+++ roles/irods_4.1.2_ubuntu/templates/setup_irods_icat.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/expect -f
+#set timeout -1
+set timeout 60
+spawn /var/lib/irods/packaging/setup_irods.sh
+
+
+expect {
+
+    -re "iRODS service account name.*:" {
+        exp_send "{{ irods_service_account_name }}\n"
+        exp_continue
+    }
+
+    -re "iRODS service group name.*:" {
+        exp_send "{{ irods_service_group_name }}\n"
+        exp_continue
+    }
+
+    -re "iRODS server's zone name.*:" {
+        exp_send "{{ irods_zone_name }}\n"
+        exp_continue
+    }
+
+    -re "iRODS server's port.*:" {
+        exp_send "{{ irods_service_port }}\n"
+        exp_continue
+    }
+
+    -re "iRODS port range .?begin.*:" {
+        exp_send "{{ irods_port_range_begin }} \n"
+        exp_continue
+    }
+
+    -re "iRODS port range .?end.*:" {
+        exp_send "{{ irods_port_range_end }}\n"
+        exp_continue
+    }
+
+    -re "iRODS Vault directory.*:" {
+        exp_send "{{ irods_vault_dir_default }}\n"
+        exp_continue
+    }
+
+    -re "iRODS server's zone_key.*:" {
+        exp_send "{{ irods_zone_key }}\n"
+        exp_continue
+    }
+
+    -re "iRODS server's negotiation_key.*:" {
+        exp_send "{{ irods_negotiation_key }}\n"
+        exp_continue
+    }
+
+    -re "Control Plane port.*:" {
+        exp_send "{{ irods_control_plane_port }}\n"
+        exp_continue
+    }
+
+    -re "Control Plane key.*:" {
+        exp_send "{{ irods_control_plane_key }}\n"
+        exp_continue
+    }
+
+    -re "Schema Validation Base URI.*:" {
+        exp_send "\n"
+        exp_continue
+    }
+
+    -re "iRODS server's administrator username.*:" {
+        exp_send "{{ irods_admin_username }}\n"
+        exp_continue
+    }
+    -re "iRODS server's administrator password:" {
+        exp_send "{{ irods_admin_password }}\n"
+        exp_continue
+    }
+
+    -re "Please confirm these settings.*:" {
+        exp_send "yes\n"
+    }
+
+    timeout {
+        send_user "\n(A) No match found.\n"
+        exit 1
+    }
+}
+
+expect {
+        -re "Database server's hostname or IP address.*:" {
+        exp_send "{{ irods_db_hostname }}\n"
+        exp_continue
+        }
+
+    -re "Database server's port.*:" {
+        exp_send "{{ irods_db_port }}\n"
+        exp_continue
+    }
+
+    -re "Database name.*:" {
+        exp_send "{{ irods_db_name }}\n"
+        exp_continue
+    }
+
+    -re "Database username.*:" {
+        exp_send "{{ irods_db_user }}\n"
+        exp_continue
+    }
+
+    -re "Database password:" {
+        exp_send "{{ irods_db_password }}\n"
+        exp_continue
+    }
+
+    -re "Please confirm these settings.*:" {
+        exp_send "yes\n"
+        exp_continue
+    }
+
+    timeout {
+        send_user "\n(B) No match found.\n"
+        exit 1
+    }
+
+    eof {
+       exit 0
+    }
+}
+expect eof
